48492153778e456dcd5f2ece3798c6a7dd29b9e4
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-09-30 12:21:46 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Nikola Anastasovski on 30.9.21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
